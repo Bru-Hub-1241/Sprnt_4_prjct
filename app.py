c0023991c169f17197803fd730b20e0275a1fff7
Bru-Hub-1241 <brooke.b.holland@gmail.com>
2025-03-04 19:37:57 -0500

    Updating

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import plotly.express as px
 st.header("Car Sales Analysis")
 
 df = pd.read_csv("vehicles_us.csv")
-#st.plotly_chart(px.histogram())
 
 if st.checkbox("Show only cars under $20,000"):
     df = df[df['price'] < 20000]
@@ -22,6 +21,13 @@ st.plotly_chart(fig)
 fig = px.scatter(df, x='model', y='model_year', color= 'paint_color', title= 'Vehicle Model Scatter Plot') 
 st.plotly_chart(fig)
 
+df['paint_color'] = df ['paint_color']. fillna('No info')
+
+df['is_4wd'] = df ['is_4wd']. fillna('unknown')
+
 st.plotly_chart(px.histogram(df, x='odometer', nbins=40, title='milage amount'))
 
 print(df[df['type'] == df['type'].max()])
+
+if st.select_slider("Show only milage under 100,000"):
+    df = df[df['count'] < 100000]
